b71e4ead09d70a63b66035cfd94aca6b2e537b0a
me-her <gajula.meher@gmail.com>
2021-03-31 11:44:09 +0530

    cors allowing all

--- index.js
+++ index.js
@@ -163,6 +163,15 @@ function generateContactsHtml(data) {
   return minifyHtml;
 }
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.post("/data", (req, res) => {
   let json_data = req.body;
 
