452ca68495df2735d9835750caf35259d4d88cbc
me-her <gajula.meher@gmail.com>
2021-03-31 19:06:46 +0530

    trying to make cors work 1

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const cors = require("cors");
 const minify = require("html-minifier").minify;
 
 app.use(express.json({ limit: "50mb" }));
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 function processData(json_data) {
@@ -163,20 +159,18 @@ function generateContactsHtml(data) {
   return minifyHtml;
 }
 
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  next();
 });
 
 app.post("/data", (req, res) => {
   let json_data = req.body;
-
-  let html = processData(json_data);
-  res.send(html);
+  let contacts = processData(json_data);
+  res.send(contacts);
 });
 
 app.listen(port, () => {
